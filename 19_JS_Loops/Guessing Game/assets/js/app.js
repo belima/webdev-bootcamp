29e4c684df496cf93eefdcc078b4b41f4f7975e1
Bê Lima <lima.bernardo@gmail.com>
2023-05-09 00:53:32 -0300

    Code cleanup & bug fixes lol

--- 19_JS_Loops/Guessing Game/assets/js/app.js
+++ 19_JS_Loops/Guessing Game/assets/js/app.js
@@ -1,57 +1,63 @@
 let forceQuit = false;
 let level = 0;
-while (level <1 || level >3 || !level) {
-    level = parseInt(prompt("Escolha um nível de dificuldade:", "1-3"));
+while (level < 1 || level > 5 || !level) {
+    level = parseInt(prompt("Escolha um nível de dificuldade.\nDigite um número de 1 a 5:"));
 }
-
+level = parseInt(level); 
 // TRANSFERIDO P/ DENTRO DO WHILE ANTERIOR
 // while (!level) {
 //     level = parseInt(prompt("Nível inválido!", "1-3"));
 // }
 
-if (level === 1) {
-    maxNum = 10;
-} else if (level === 2) {
-    maxNum = 30;
+if (level % 2 === 0) {
+    maxNum = (10 ** level) / 2;
 } else {
-    maxNum = 100;
+    maxNum = 10 ** level;
 }
 
 secretNum = Math.floor(Math.random() * (maxNum + 1));
 
-let guess = prompt(`Escolha um número entre 0 e ${maxNum}:`, `1 - ${maxNum}`);
+let guess = prompt(`Escolha um número entre 0 e ${maxNum}:\ndigite 'q' para sair.`);
 let attempts = 1;
 
 while (parseInt(guess) !== secretNum) {
- 
+
     if (guess === "q") {
         forceQuit = true;
         break;
     }
     guess = parseInt(guess);
-    attempts++;
-    if (guess < secretNum) {
+
+    if (guess < 0 || guess > maxNum) {
+        guess = prompt(`Aloow! Entre 0 e ${maxNum}!\nVou contar essa tentativa pra tu ficar esperto!\ndigite 'q' para sair.`);
+        attempts++;
+    } else if (guess < secretNum) {
         if (secretNum - guess <= 3) {
-            guess = prompt(`Uuuui! Quase! Maior um pouquinho!`);
+            guess = prompt(`Uuuui! Quase! Maior um pouquinho!\ndigite 'q' para sair.`);
+            attempts++;
         } else {
-            guess = prompt(`Errou! Chutou baixo!.`);
+            guess = prompt(`Errou! Chutou baixo!.\ndigite 'q' para sair.`);
+            attempts++;
         }
-    } else
-    if (guess > secretNum) {
+    } else if (guess > secretNum) {
         if (guess - secretNum <= 3) {
-            guess = prompt(`Uuuui! Quase! Menor um pouquinho!`);
+            guess = prompt(`Uuuui! Quase! Menor um pouquinho!\ndigite 'q' para sair.`);
+            attempts++;
         } else {
-            guess = prompt(`Errou! Alto demais!`);
+            guess = prompt(`Errou! Alto demais!\ndigite 'q' para sair.`);
+            attempts++;
         }
+    } else {
+        guess = prompt(`Tem que ser um número né?!Essa vou deixar passar\ndigite 'q' para sair.`);
     }
 }
 
 if (forceQuit) {
     alert("Ok, quitter. C ya.")
-} else{
+} else {
     if (attempts === 1) {
         alert(`Acertôô, mizeraavi! De primeira, carai!`);
     } else {
         alert(`Acertôô, mizeraavi! Chutou ${attempts} vezes!`);
-    }    
+    }
 }
